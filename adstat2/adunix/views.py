d7c7b8093e10facf8bd0cf40fad53820c8497989
Gogs <r2d2-lex@yandex.ru>
2024-12-11 14:08:09 +0500

    Добавление основных полей для атрибутов пользователя + сортировка групп и пользователей

--- adstat2/adunix/views.py
+++ adstat2/adunix/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
 from django.conf import settings
 from loguru import logger as logging
-import sys
-from .ldap_manager import LdapManager, GROUP_SEARCH_FILTER, OU_SEARCH_FILTER, USER_SEARCH_FILTER
+from .ldap_manager import LdapManager
 
 
 def index(request):
     logging.debug(f'SETTINGS "{settings.LDAP_SERVER}", "{settings.USERNAME}", "{settings.PASSWORD}", "{settings.BASE_DN_ROOT}"')
 
     with LdapManager(settings.LDAP_SERVER, settings.USERNAME, settings.PASSWORD, settings.BASE_DN_ROOT) as ldap_manger:
+        # Получение информации о группах
         attribute_list = ['gidNumber', 'description']
         group_result = ldap_manger.get_groups_list(attribute_list)
+        group_result = sorted(group_result, key=lambda x: x['cn'])
+
+        # # Получение информации о пользователях
+        attribute_list = ['cn', 'uid', 'msSFU30Name', 'msSFU30NisDomain', 'uidNumber', 'gidNumber', 'loginShell',
+                          'unixHomeDirectory']
+        users_result = ldap_manger.get_users_list(attribute_list)
+        users_result = sorted(users_result, key=lambda x: x['sAMAccountName'])
 
         context = {
             'groups': group_result,
+            'users': users_result,
         }
     return render(request, 'adunix/index.html', context)
