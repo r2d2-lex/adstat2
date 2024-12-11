9cae860fc306eb693a388a88f30b93e8e30bbf24
Gogs <r2d2-lex@yandex.ru>
2024-12-11 10:52:48 +0500

    Получаем список групп в select

--- adstat2/adunix/views.py
+++ adstat2/adunix/views.py
@@ -6,19 +6,13 @@ from .ldap_manager import LdapManager, GROUP_SEARCH_FILTER, OU_SEARCH_FILTER, US
 
 
 def index(request):
-    # logging.remove(0)
-    logging.add(sys.stderr, level=settings.LOGGING_LEVEL)
-
-    print(f'SETTINGS "{settings.LDAP_SERVER}", "{settings.USERNAME}", "{settings.PASSWORD}", "{settings.BASE_DN_ROOT}"')
+    logging.debug(f'SETTINGS "{settings.LDAP_SERVER}", "{settings.USERNAME}", "{settings.PASSWORD}", "{settings.BASE_DN_ROOT}"')
 
     with LdapManager(settings.LDAP_SERVER, settings.USERNAME, settings.PASSWORD, settings.BASE_DN_ROOT) as ldap_manger:
         attribute_list = ['gidNumber', 'description']
         group_result = ldap_manger.get_groups_list(attribute_list)
-        if group_result:
-            for group in group_result:
-                group_cn = group.get('cn')
-                group_gid = group.get('gidNumber', '')
-                group_description = group.get('description', '')
-                print(f'Группа: {group_cn}, Gid: {group_gid}, Описание: {group_description}')
 
-    return render(request, 'adunix/index.html')
+        context = {
+            'groups': group_result,
+        }
+    return render(request, 'adunix/index.html', context)
