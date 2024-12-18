3f6ebc553c30ee964f955f5906470a31df3d11fb
Gogs <r2d2-lex@yandex.ru>
2024-12-18 14:26:34 +0500

    Django secret key генерация

--- adstat2/adstat2/settings.py
+++ adstat2/adstat2/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+from django.core.management.utils import get_random_secret_key
 from loguru import logger as logging
 from pathlib import Path
 import environ
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u&0o5jf4u!@@e)9v98w28pvet3)d)b1f!2*2__n49u1w1c6ly7'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
