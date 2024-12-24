32f4319a1460312ead453054c1c9c00da7064417
Gogs <r2d2-lex@yandex.ru>
2024-12-24 09:34:42 +0500

    fix user_dn

--- adstat2/adunix/tests/test_ldap_manager.py
+++ adstat2/adunix/tests/test_ldap_manager.py
@@ -11,13 +11,13 @@ class TestLdapManager(unittest.TestCase):
     def setUp(self, mock_connection, mock_server):
         self.mock_server = mock_server.return_value
         self.mock_connection = mock_connection.return_value
-        self.ldap_manager = LdapManager('hostname', 'username', 'password', 'base_dn')
+        self.ldap_manager = LdapManager('hostname', 'username', 'password', 'DC=domain,DC=com')
         self.ldap_manager.connection = self.mock_connection
 
     def test_update_user_values_success(self):
         self.mock_connection.result = {'result': 0, 'description': 'success'}
         attributes = {'displayName': 'New Name'}
-        result, status = self.ldap_manager.update_user_values('user_dn', attributes)
+        result, status = self.ldap_manager.update_user_values('CN=TestUser,OU=Users,OU=TEST_GROUP,DC=DOMAIN,DC=COM', attributes)
 
         self.assertTrue(result)
         self.assertIn('Атрибут displayName успешно изменен на New Name.', status)
@@ -25,7 +25,7 @@ class TestLdapManager(unittest.TestCase):
     def test_update_user_values_failure(self):
         self.mock_connection.result = {'result': 1, 'description': 'error'}
         attributes = {'displayName': 'New Name'}
-        result, status = self.ldap_manager.update_user_values('user_dn', attributes)
+        result, status = self.ldap_manager.update_user_values('CN=TestUser,OU=Users,OU=TEST_GROUP,DC=DOMAIN,DC=COM', attributes)
 
         self.assertFalse(result)
         self.assertIn('Ошибка при изменении атрибута displayName:', status)
