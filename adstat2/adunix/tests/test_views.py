8e50036e8ae4b9a2579e0e5c2e7593f0137b2ebe
Gogs <r2d2-lex@yandex.ru>
2024-12-24 16:26:19 +0500

    Тесты для index view+ get_new_uid

--- adstat2/adunix/tests/test_views.py
+++ adstat2/adunix/tests/test_views.py
@@ -1,6 +1,19 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.test import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
+from django.urls import reverse
+
+
+class UserTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username='testuser', email='testuser@domain.com', password='testpass', is_staff=True
+        )
+
+    def setUp(self):
+        self.client.force_login(self.user)
 
 
 class TestRegister(TestCase):
@@ -10,12 +23,7 @@ class TestRegister(TestCase):
         self.assertRedirects(response, '/admin/login/?next=/')
 
 
-class TestIndex(TestCase):
-
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', email='testuser@domain.com', password='testpass', is_staff=True)
-        self.client.force_login(self.user)
+class TestIndex(UserTestCase):
 
     @patch('adunix.views.LdapManager')
     def test_index_view(self, MockLdapManager):
@@ -29,7 +37,7 @@ class TestIndex(TestCase):
             {'sAMAccountName': 'user2', 'uidNumber': 1002, 'cn': 'User Two'},
         ]
 
-        response = self.client.get('/')
+        response = self.client.get(reverse('adunix:index'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'adunix/index.html')
@@ -41,3 +49,25 @@ class TestIndex(TestCase):
         self.assertEqual(len(response.context['groups']), 2)
         self.assertEqual(len(response.context['users']), 2)
         self.assertEqual(response.context['max_uid'], 1002)
+
+
+class GetNewUidViewTests(UserTestCase):
+
+    @patch('adunix.views.LdapManager')
+    def test_get_new_uid(self, MockLdapManager):
+        mock_ldap_manager = MockLdapManager.return_value.__enter__.return_value
+        mock_ldap_manager.get_users_list.return_value = [
+            {'uidNumber': 1001},
+            {'uidNumber': 1002},
+            {'uidNumber': 1003},
+        ]
+
+        response = self.client.post(reverse('adunix:get_new_uid'))
+
+        self.assertEqual(response.status_code, 200)
+
+        json_response = response.json()
+        self.assertEqual(json_response['uidNumber'], 1004)
+        self.assertEqual(json_response['domain'], settings.DOMAIN)
+
+        mock_ldap_manager.get_users_list.assert_called_once_with(['uidNumber'])
