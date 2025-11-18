e81121f9bb28fd77e84a469d7e36403fafe1e1b7
Gogs <r2d2-lex@yandex.ru>
2025-11-18 11:10:29 +0500

    удаляем неиспользуемый роутер

--- adstat2/adunix/urls.py
+++ adstat2/adunix/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import index, get_user_data, update_user_data, delete_user_data, get_new_uid, get_group_data, \
-    update_group_data
+from .views import index, get_user_data, update_user_data, delete_user_data, get_new_uid, get_group_data
 
 app_name = 'adunix'
 urlpatterns = [
@@ -8,7 +7,6 @@ urlpatterns = [
     path('get_user_data/', get_user_data, name='get_user_data'),
     path('get_group_data/', get_group_data, name='get_group_data'),
     path('update_user_data/', update_user_data, name='update_user_data'),
-    path('update_group_data/', update_group_data, name='update_group_data'),
     path('delete_user_data/', delete_user_data, name='delete_user_data'),
     path('get_new_uid/', get_new_uid, name='get_new_uid'),
 ]
