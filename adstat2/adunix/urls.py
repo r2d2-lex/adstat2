7ae1c2cb654d1f041d1007c93af2a3a32376d4be
Gogs <r2d2-lex@yandex.ru>
2025-11-18 17:06:45 +0500

    Удаляем ненужный get_group_data

--- adstat2/adunix/urls.py
+++ adstat2/adunix/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import index, get_user_data, update_user_data, delete_user_data, get_new_uid, get_group_data
+from .views import index, get_user_data, update_user_data, delete_user_data, get_new_uid
 
 app_name = 'adunix'
 urlpatterns = [
     path('', index, name='index'),
     path('get_user_data/', get_user_data, name='get_user_data'),
-    path('get_group_data/', get_group_data, name='get_group_data'),
     path('update_user_data/', update_user_data, name='update_user_data'),
     path('delete_user_data/', delete_user_data, name='delete_user_data'),
     path('get_new_uid/', get_new_uid, name='get_new_uid'),
